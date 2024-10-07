38b78a8ec805fde272c0faac1aa954aa1bc83302
dohaelsawy <dohaelsawy18@gmail.com>
2024-10-07 14:06:36 +0300

    feat: add note model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    url = models.CharField()
+    content = models.CharField()
+    is_read = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now=True)
+    num_of_views = models.IntegerField(default=0)
+
