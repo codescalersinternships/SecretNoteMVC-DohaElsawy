f367fd7c69dd0f11b2c333567c4ba8951ad6d960
dohaelsawy <dohaelsawy18@gmail.com>
2024-10-07 14:25:27 +0300

    fix: model name settings in installed app

--- note/models.py
+++ note/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Note(models.Model):
-    url = models.CharField()
-    content = models.CharField()
+    url = models.CharField(max_length=500)
+    content = models.CharField(max_length=500)
     is_read = models.BooleanField(default=False)
     start_date = models.DateTimeField(auto_now=True)
     num_of_views = models.IntegerField(default=0)
