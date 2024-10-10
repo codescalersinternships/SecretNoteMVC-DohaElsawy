1172455d2e1d36923a9734a6186f2297b6fbb84c
dohaelsawy <dohaelsawy18@gmail.com>
2024-10-10 14:17:06 +0300

    feat: add login

--- secret_note/urls.py
+++ secret_note/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from note import urls as note_url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('note/',include(note_url))
+    path('note/',include(note_url)),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="note/home.html"), name="home"),
 ]
